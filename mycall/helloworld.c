11f7300b1132752efcff5032d3396ec7c607737d
chialunglee <105502539@cc.ncu.edu.tw>
2019-12-04 14:52:41 +0800

    Output PID

---
+++ mycall/helloworld.c
@@ -0,0 +1,8 @@
+#include <linux/kernel.h>
+#include <linux/sched.h>
+
+asmlinkage void linux_survey_TT(unsigned long *result) {
+	printk("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+	struct task_struct *ts = current;
+	printk("%d\n", ts->pid);
+}
