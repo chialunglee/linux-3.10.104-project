30d390506fe18e7d031ade12bfc7d8d78feac09b
chialunglee <105502539@cc.ncu.edu.tw>
2019-12-17 01:14:16 +0800

    vm_area_struct

--- mycall/helloworld.c
+++ mycall/helloworld.c
@@ -2,7 +2,7 @@
 #include <linux/sched.h>
 
 asmlinkage void linux_survey_TT(unsigned long *result) {
-	printk("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+	// printk("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
 	struct task_struct *ts = current;
-	printk("%d\n", ts->pid);
+	printk("%d\n", ts->mm->mmap);
 }
