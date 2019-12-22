b715aa22e295378d61c474c313f97b11d0a3a28b
chialunglee <105502539@cc.ncu.edu.tw>
2019-12-22 21:08:01 +0800

    print vm_start before vm_end, add array assignment

--- mycall/helloworld.c
+++ mycall/helloworld.c
@@ -5,7 +5,11 @@ asmlinkage void linux_survey_TT(unsigned long *result) {
 	struct task_struct *ts = current;
 	struct vm_area_struct *vma = ts->mm->mmap;
 	do {
-		printk("%lx\n", vma->vm_end);
 		printk("%lx\n", vma->vm_start);
+		*result = vma->vm_start;
+		result += 1;
+		printk("%lx\n", vma->vm_end);
+		*result = vma->vm_end;
+		result += 1;
 	}while(vma->vm_next && (vma = vma->vm_next));
 }
