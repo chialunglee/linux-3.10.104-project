8069ca4e3c742266bd5b3576e603a50d1812bf5f
chialunglee <105502539@cc.ncu.edu.tw>
2020-01-14 23:36:44 +0800

    從 Suer Va 取得改動
    顯示改成每個process自己最外層的pid、修正抓程序自己pid

--- get_process_session_group/get_process_session_group.c
+++ get_process_session_group/get_process_session_group.c
@@ -3,31 +3,23 @@
 #include<linux/sched.h>
 #include<linux/delay.h>
 #include<asm-generic/delay.h>
-#include<linux/syscalls.h>
-#include<linux/module.h>
-#include<linux/fs.h>
-#include<linux/proc_fs.h>
-#include<linux/mm.h>
-#include<linux/mm_types.h>
-#include<linux/highmem.h>
-#include<linux/string.h>
-#include<asm/io.h>
 
 asmlinkage int get_process_session_group(unsigned int *ptr, int SIZE) {
 	struct task_struct *empty;
 	struct task_struct *ts = current;
 	int current_session_id = ts->group_leader->pids[PIDTYPE_SID].pid->numbers[0].nr;
 	int counter = 0;
-	int level = ts->pids[0].pid->level;
+	
 	for_each_process(empty) {
 		if(empty->group_leader->pids[PIDTYPE_SID].pid->numbers[0].nr == current_session_id) {
+			int level = empty->pids[0].pid->level;
 			counter += 1;
 			if(counter <= SIZE) {
-				*ptr = empty->group_leader->pids[PIDTYPE_PID].pid->numbers[level].nr;
+				*ptr = empty->pids[PIDTYPE_PID].pid->numbers[level].nr;
 				ptr += 1;
 			}
 		}
 	}
-	
+	msleep(1000);
 	return counter;
 }
